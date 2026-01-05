b907b44e2a12f2846c1706c164e444da82092889
Sachdeva, Kapil <ksachdeva17@gmail.com>
2026-01-05 09:12:50 -0600

    chore: add .clang-format (copied from zephyr project) and format main.c

--- example-app/src/main.c
+++ example-app/src/main.c
@@ -9,33 +9,34 @@ LOG_MODULE_REGISTER(main);
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-int main(void) {
-  int ret;
-  bool led_state = true;
+int main(void)
+{
+	int ret;
+	bool led_state = true;
 
-  if (!gpio_is_ready_dt(&led)) {
-    LOG_ERR("Error, gpio is not ready\n");
-    return 0;
-  }
-  
-  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-  if (ret < 0) {
-    LOG_ERR("Error configuring gpio: %d\n", ret);
-    return 0;
-  }
-  
-  LOG_DBG("LED configured and ready to blink\n");
-  while (1) {
-    ret = gpio_pin_toggle_dt(&led);
-    if (ret < 0) {
-      LOG_ERR("Error toggling led\n");
-      return 0;
-    }
+	if (!gpio_is_ready_dt(&led)) {
+		LOG_ERR("Error, gpio is not ready\n");
+		return 0;
+	}
 
-    led_state = !led_state;
-    printf("LED is now: %s\n", led_state ? "ON" : "OFF");
-    
-    k_msleep(1000);
-  }
-  return 0;
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Error configuring gpio: %d\n", ret);
+		return 0;
+	}
+
+	LOG_DBG("LED configured and ready to blink\n");
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			LOG_ERR("Error toggling led\n");
+			return 0;
+		}
+
+		led_state = !led_state;
+		printf("LED is now: %s\n", led_state ? "ON" : "OFF");
+
+		k_msleep(1000);
+	}
+	return 0;
 }
